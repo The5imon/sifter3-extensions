315277bd9d41c013e11728b37418f09070b80cb8
Manfred Kaiser <manfred.kaiser@logfile.at>
2020-10-17 19:37:28 +0200

    removed sifter files, keeping only unofficial extensions

--- sifter_extensions/extension.py
+++
@@ -1,6 +0,0 @@
-import sifter.handler
-
-__all__ = ('register',)
-
-def register(extension_name):
-    sifter.handler.register('extension', extension_name, True)
--- sifter_extensions/grammar/validator.py
+++
@@ -1,7 +0,0 @@
-__all__ = ('Validator',)
-
-class Validator(object):
-
-    def validate(self, arg_list, starting_index):
-        raise NotImplementedError
-
